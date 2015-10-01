fbf4cc22a595696b39dde6e702632c08ec5ba922
Carl Vuorinen <carl.vuorinen@gmail.com>
2015-10-01 21:49:38 +0300

    Example how to take a picture

---
+++ examples/01-take-picture.php
@@ -0,0 +1,18 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Cvuorinen\Raspicam\Raspistill;
+
+$camera = new Raspistill();
+
+$filename = date('Y-m-d_H:i:s') . '.jpg';
+
+try {
+    $camera->takePicture($filename);
+
+    echo 'Saved picture to ' . $filename . "\n";
+
+} catch (\Exception $e) {
+    echo 'ERROR: ' . $e->getMessage() . "\n";
+}
