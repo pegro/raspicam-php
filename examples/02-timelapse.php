d173a81a3d56f20d4e58cdd2d06bb6bde8574128
Carl Vuorinen <carl.vuorinen@gmail.com>
2015-11-07 08:59:16 +0200

    Update timelapse example

--- examples/02-timelapse.php
+++ examples/02-timelapse.php
@@ -11,11 +11,14 @@ $camera->flip()
     ->ISO(600)
     ->quality(50);
 
+// Note you should specify %04d at the point in the filename where you want a frame count number to appear.
+// e.g. 'image%04d.jpg' will save files with names image0001.jpg, image0002.jpg, image003.jpg etc.
+
 // take picture every ten seconds for two minutes
 $camera->startTimelapse('image%04d.jpg', 10, 120);
 
 // Timelapse: take picture every five minutes for 24 hours
-$camera->startTimelapse('image%04d.jpg', 5, 1440, Raspistill::TIMEUNIT_MINUTE);
+$camera->startTimelapse('timelapse%04d.jpg', 5, 1440, Raspistill::TIMEUNIT_MINUTE);
 
 // Burst: take pictures as fast as possible (~ 30ms interval) for three seconds
 $camera->startTimelapse('burst%04d.jpg', 0, 3);
